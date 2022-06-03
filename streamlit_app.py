1bb2f8ec33625c42f6f353c91ce471c382c92dee
G-CodeRepo <gjabut@yahoo.com>
2022-06-02 15:06:41 -1000

    Update streamlit_app.py
    cleanup code to consolidate adding a fruit to a function and  to actually insert the fruit entry into the database table

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,7 +56,29 @@ if streamlit.button('Get Fruit Load List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   my_data_rows = get_fruit_load_list()
   streamlit.dataframe(my_data_rows)
+  
+  
+  
+#############################################################  
 
-# Add fruit choice text entry box
-add_my_fruit = streamlit.text_input('What fruit would you like to add?', 'kiwi');
+# Allow the end user to add a fruit to the list
 streamlit.write('Thanks for adding', add_my_fruit)
+
+#this will not work correctly, but just go with it for now
+
+
+
+##################################################
+
+# Allow the end user to add a fruit to the list
+def insert_row_snowflake(new_fruit):
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("insert into fruit_load_list values ('" + new_fruit + "')")
+    return "Thanks for adding " + new_fruit
+  
+add_my_fruit = streamlit.text_input('What fruit would you like to add?');
+if streamlit.button('Add a Fruit to the List'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  back_from_function = insert_row_snowflake(add_my_fruit)
+  streamlit.text(back_from_function)
+
